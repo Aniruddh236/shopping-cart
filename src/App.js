735a2d03b2a85e60839fa09515ef8dda5a524055
Aniruddhsinh236 <2018csani068@ldce.ac.in>
2024-09-10 11:21:53 +0530

    added hashrouter instead simple router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { HashRouter as Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Home from './components/Home';
