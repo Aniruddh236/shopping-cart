f21a6884be86ad01d1cd5df4ca4c84803738a52d
Aniruddhsinh236 <2018csani068@ldce.ac.in>
2024-09-10 11:25:46 +0530

    updating cart to  shopping-cart in route

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <>
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='cart' element={<ShoppingCart />} />
+      <Route path='shopping-cart' element={<ShoppingCart />} />
     </Routes>
 
     <Outlet />
